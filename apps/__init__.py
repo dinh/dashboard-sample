0bd27533c41ba2111400b18e003dbe9dfb28c996
Zak May <zak@oldwellpartners.com>
2022-10-13 06:02:21 -0400

    handle running out of API tokens

--- apps/__init__.py
+++ apps/__init__.py
@@ -4,12 +4,17 @@ from .config import Config
 
 
 def get_404(e):
-    return render_template("home/page-404.html"), 404
+    return render_template("errors/404.html"), 404
+
+
+def get_500(e):
+    return render_template("errors/404.html"), 500
 
 
 application = Flask(__name__)
 
 application.register_error_handler(404, get_404)
+application.register_error_handler(500, get_500)
 
 application.config.from_object(Config)
 
