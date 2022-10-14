fea50e4490bc724e65797f7afca60cd79ae24601
Zak May <zak@oldwellpartners.com>
2022-10-14 07:17:04 -0400

    wrap endpoints for more generic query and easier to add additional endpoints

--- apps/alpha_vantage/urls.py
+++ apps/alpha_vantage/urls.py
@@ -1,4 +1,24 @@
+import inspect
 import os
+from functools import wraps
+from urllib.parse import urlencode
+
+
+def endpoint_builder(function_param=None):
+    def _endpoint_url(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            url = func(*args, **kwargs)
+            kwargs['function'] = function_param
+            kwargs['apikey'] = args[0].api_key
+            for value, arg in zip(args, inspect.getfullargspec(func).args):
+                if arg != 'self':
+                    kwargs[arg] = value
+            return url + urlencode(kwargs)
+
+        return wrapper
+
+    return _endpoint_url
 
 
 class StockURLs:
@@ -7,27 +27,29 @@ class StockURLs:
     def __init__(self, key=None, bucket=None):
         self.api_key = key if key else os.getenv('STOCK_API_KEY')
         self.bucket = bucket if bucket else os.getenv('DATA_BUCKET')
-        self._api_url = "https://www.alphavantage.co/query?function={function}&" \
-                        "symbol={symbol}&apikey={key}&outputsize={output}"
+        self._base_api_url = "https://www.alphavantage.co/query?"
         self._bucket_url = "s3://{bucket}/{function}/{symbol}"
 
-    def get_market_stock_url_s3(self):
-        _FUNCTION = 'TIME_SERIES_DAILY'
-        return self._bucket_url.format(bucket=self.bucket, function=_FUNCTION, symbol='MARKET_DATA')
-
+    @endpoint_builder(function_param='TIME_SERIES_DAILY')
     def get_daily_stock_series_url(self, symbol, output='compact'):
-        _FUNCTION = 'TIME_SERIES_DAILY'
-        return self._api_url.format(function=_FUNCTION, symbol=symbol, key=self.api_key, output=output)
+        return self._base_api_url
+
+    @endpoint_builder(function_param='TIME_SERIES_DAILY_ADJUSTED')
+    def get_daily_adjusted_stock_series_url(self, symbol, output='compact'):
+        return self._base_api_url
+
+    @endpoint_builder(function_param='SYMBOL_SEARCH')
+    def get_search_url(self, keywords):
+        return self._base_api_url
+
+    @endpoint_builder(function_param='NEWS_SENTIMENT')
+    def get_news_url(self, tickers):
+        return self._base_api_url
 
     def get_daily_stock_series_url_s3(self, symbol):
         _FUNCTION = 'TIME_SERIES_DAILY'
         return self._bucket_url.format(bucket=self.bucket, function=_FUNCTION, symbol=symbol)
 
-    def get_daily_adjusted_stock_series_url(self, symbol, output='compact'):
-        _FUNCTION = 'TIME_SERIES_DAILY_ADJUSTED'
-        return self._api_url.format(function=_FUNCTION, symbol=symbol, key=self.api_key, output=output)
-
-    def get_search_url(self, search_string):
-        _FUNCTION = 'SYMBOL_SEARCH'
-        return 'https://www.alphavantage.co/query?function={function}&keywords={keyword}&apikey={api_key}'.format(
-            function=_FUNCTION, keyword=search_string, api_key=self.api_key)
+    def get_market_stock_url_s3(self):
+        _FUNCTION = 'TIME_SERIES_DAILY'
+        return self._bucket_url.format(bucket=self.bucket, function=_FUNCTION, symbol='MARKET_DATA')
