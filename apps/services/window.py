18f1fde74957c4ab9bc929918563c37816728b7f
Zak May <a.zakmay@gmail.com>
2022-10-22 07:14:41 -0400

    make windows more precise

--- apps/services/window.py
+++ apps/services/window.py
@@ -1,27 +1,48 @@
 import datetime
+from functools import partial
 
-import numpy as np
 import pandas as pd
 
-_1MONTH = 21
-_1YEAR = 260
 
-LOOKBACK = {
-    '1D': 1,
-    '5D': 5,
-    '1M': _1MONTH,
-    '3M': _1MONTH * 3,
-    '6M': _1MONTH * 6,
-    '1Y': _1YEAR,
-    '3Y': _1YEAR * 3,
-    '5Y': _1YEAR * 5,
-    '10Y': _1YEAR * 10,
-}
+def lookback_n_days(end_dt, n):
+    return end_dt - pd.DateOffset(days=n)
+
+
+def lookback_n_months(end_dt, n):
+    return end_dt - pd.DateOffset(months=n)
+
+
+lookback_1_day = partial(lookback_n_days, n=1)
+lookback_5_day = partial(lookback_n_days, n=5)
+lookback_1_month = partial(lookback_n_months, n=1)
+lookback_3_month = partial(lookback_n_months, n=3)
+lookback_6_month = partial(lookback_n_months, n=6)
+lookback_1_year = partial(lookback_n_months, n=12)
+lookback_3_year = partial(lookback_n_months, n=12 * 3)
+lookback_5_year = partial(lookback_n_months, n=12 * 5)
+lookback_10_year = partial(lookback_n_months, n=12 * 10)
 
 
 def get_start_date(date, window='10Y'):
+    if window == '1D':
+        return lookback_1_day(date)
+    if window == '5D':
+        return lookback_5_day(date)
+    if window == '1M':
+        return lookback_1_month(date)
+    if window == '3M':
+        return lookback_3_month(date)
+    if window == '6M':
+        return lookback_6_month(date)
     if window == 'YTD':
         return pd.to_datetime(datetime.datetime(date.year, 1, 1))
+    if window == '1Y':
+        return lookback_1_year(date)
+    if window == '3Y':
+        return lookback_3_year(date)
+    if window == '5Y':
+        return lookback_5_year(date)
+    if window == '10Y':
+        return lookback_10_year(date)
     else:
-        start_date = np.busday_offset(np.busday_offset(date.date(), -1), -LOOKBACK.get(window))
-        return start_date
+        raise Exception('Not a valid window')
